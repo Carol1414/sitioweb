fb406a875f5bb954096116fc17f4c820e88615c0
Carol1414 <85178309+Carol1414@users.noreply.github.com>
2021-06-01 09:37:59 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function contActive(){
+    var selector = document.getElementById("cont-colors");
+    var selector2 = document.getElementById("btn-elemento");
+    selector.classList.toggle("active");
+    selector2.classList.toggle("active");
+}
+
+function bgpurple(){
+    var selector = document.getElementById("body");
+    selector.classList.toggle("purple");
+}
+
+function bgblue(){
+    var selector = document.getElementById("body");
+    selector.classList.toggle("blue");
+}
+
+function bgcian(){
+    var selector = document.getElementById("body");
+    selector.classList.toggle("cian");
+}
+
+function bgblack(){
+    var selector = document.getElementById("body");
+    selector.classList.toggle("black");
+}
+
+function bgbeige(){
+    var selector = document.getElementById("body");
+    selector.classList.toggle("beige");
+}
